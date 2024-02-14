b1baf74f4862784611880a92f5fd123c6e15d8b7
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-14 11:38:16 -0500

    zx build scripts

--- apps/mobile/appcenter-post-clone.sh
+++ apps/mobile/appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd $APPCENTER_SOURCE_DIRECTORY
 yarn install
-zx --install tools/scripts/appcenter/post-clone/post-clone.mjs
+yarn zx --install tools/scripts/appcenter/post-clone/post-clone.mjs
 
 # Add app specific post-build commands here
