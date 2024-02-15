19d3cf12d3d71958323db829a8226246f24ce9ed
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-15 18:02:21 -0500

    fix app post-clone

--- apps/mobile/appcenter-post-clone.sh
+++ apps/mobile/appcenter-post-clone.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH=$(dirname $(realpath $0))
-
 cd $APPCENTER_SOURCE_DIRECTORY/tools/scripts/appcenter
 npm i
 npx zx post-clone/post-clone.mjs
 
 # Add app specific post-build commands here
 
-cd $PROJECT_PATH
+cd $(dirname $APPCENTER_REACTNATIVE_PACKAGE)
 npx zx appcenter/post-clone.mjs
