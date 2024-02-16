89c6b0c9d309edd852934b369c909fd3b25fb558
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-15 23:19:36 -0500

    cocoapods fix

--- tools/scripts/appcenter/post-clone/post-clone.mjs
+++ tools/scripts/appcenter/post-clone/post-clone.mjs
@@ -6,8 +6,19 @@ console.log(chalk.bgGreen('[Post-Clone Step] Nx + React Native + Appcenter'));
 logRunInfo();
 
 cd(envVars.appCenter.sourceDirectory);
+
+// CocoaPods v1.15.0 is broken for hermes-engine
+// Appcenter uses v1.15.0 by default so we need to update it to the latest version
+await $`which pod`;
+await $`brew update`;
+await $`brew install cocoapods`;
+
+// Install dependencies
 await $`yarn install`;
+
+// Make AppCenter use yarn
 await $`cp yarn.lock ./${envVars.nx.projectDir}/yarn.lock`;
+
+// Sync dependencies and ensure symlink to node_modules
 await $`yarn nx sync-deps ${envVars.nx.project}`;
 await $`yarn nx ensure-symlink ${envVars.nx.project}`;
-cd(envVars.nx.projectDir);
