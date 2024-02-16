ca9b03a27ac8c9549d1a1aef8cccda819ecd615d
Sebastian Duque Gutierrez <13395979+sebastiandg7@users.noreply.github.com>
2024-02-15 20:02:03 -0500

    fix app post-clone

--- apps/mobile/appcenter/post-clone.mjs
+++ apps/mobile/appcenter/post-clone.mjs
@@ -1,9 +1,10 @@
 #!/usr/bin/env zx
+import { cd } from 'zx/core';
 import { envVars } from './env.mjs';
 
 // Nothing special here, just a simple script to show the project configuration
 // and custom environment variable for reference.
-
+cd(envVars.appCenter.sourceDirectory);
 const nxProject = JSON.parse(
   (await $`npx nx show project ${envVars.nx.project} --json`.quiet()).stdout
 );
